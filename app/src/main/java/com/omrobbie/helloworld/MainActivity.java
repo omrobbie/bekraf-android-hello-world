d83c68085f57288dd2ca1fb7ed683d401c97f5c0
omrobbie <putu.roby@yahoo.com>
2017-05-08 20:57:56 +0800

    tampilkan tulisan hello dengan Toast

--- app/src/main/java/com/omrobbie/helloworld/MainActivity.java
+++ app/src/main/java/com/omrobbie/helloworld/MainActivity.java
@@ -2,6 +2,11 @@ package com.omrobbie.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
+        EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("user", "Hello World!");
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
