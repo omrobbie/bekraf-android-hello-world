123f052aa14753cfc9709ee9d01c5fe715a3db56
omrobbie <putu.roby@yahoo.com>
2017-05-15 20:18:23 +0800

    manfaatkan fungsi intent untuk latihan sebelumnya, layout login

---
+++ app/src/main/java/com/omrobbie/helloworld/RelativeLoginTarget.java
@@ -0,0 +1,13 @@
+package com.omrobbie.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLoginTarget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_login_target);
+    }
+}
