b08c3d8b4ad507bc4b921426ee213abc20eec62a
omrobbie <putu.roby@yahoo.com>
2017-05-15 19:24:22 +0800

    tampilkan nilai dari username dengan toast

--- app/src/main/java/com/omrobbie/helloworld/IntentActivity.java
+++ app/src/main/java/com/omrobbie/helloworld/IntentActivity.java
@@ -2,8 +2,10 @@ package com.omrobbie.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class IntentActivity extends AppCompatActivity {
 
@@ -12,7 +14,16 @@ public class IntentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
 
-        EditText txtName = (EditText) findViewById(R.id.txtName);
+        final EditText txtName = (EditText) findViewById(R.id.txtName);
         Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String userName = txtName.getText().toString();
+
+                Toast.makeText(IntentActivity.this, userName, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
