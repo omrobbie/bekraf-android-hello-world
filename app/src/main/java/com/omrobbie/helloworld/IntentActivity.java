dc4e4526f61d96ced3f41597f3d027b2c2fc6df8
omrobbie <putu.roby@yahoo.com>
2017-05-17 20:09:09 +0800

    simpan data preference, dan bikin tombol logout

--- app/src/main/java/com/omrobbie/helloworld/IntentActivity.java
+++ app/src/main/java/com/omrobbie/helloworld/IntentActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import static android.R.attr.data;
 
 public class IntentActivity extends AppCompatActivity {
 
@@ -21,13 +24,15 @@ public class IntentActivity extends AppCompatActivity {
         final EditText txtName = (EditText) findViewById(R.id.txtName);
         Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
+        // deklarasi SharedPreferences
+        pref = getBaseContext().getSharedPreferences("userprefs", MODE_PRIVATE);
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String userName = txtName.getText().toString();
 
                 // simpan data kedalam session (sharedpreference)
-                pref = getBaseContext().getSharedPreferences("userprefs", MODE_PRIVATE);
                 pref.edit().putString("username", userName).commit();
 
                 // jalankan fungsi gotoHome
@@ -36,7 +41,7 @@ public class IntentActivity extends AppCompatActivity {
         });
 
         // cek apakah ada userName di sharedpreference
-        String datauser = pref.getString("userName", null);
+        String datauser = pref.getString("username", null);
 
         if (datauser != null) {
             gotoHome(datauser);
@@ -48,5 +53,6 @@ public class IntentActivity extends AppCompatActivity {
         Intent intent = new Intent(IntentActivity.this, IntentTargetActivity.class);
         intent.putExtra("data", userName);
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/omrobbie/helloworld/IntentTargetActivity.java
+++ app/src/main/java/com/omrobbie/helloworld/IntentTargetActivity.java
@@ -1,11 +1,16 @@
 package com.omrobbie.helloworld;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class IntentTargetActivity extends AppCompatActivity {
+
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,20 @@ public class IntentTargetActivity extends AppCompatActivity {
         // tampilkan data kiriman dari layout pertama
         String userName = getIntent().getStringExtra("data");
         txtIntentTarget.setText(userName);
+
+        // logout, dan kembali meminta username
+        Button btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pref = getBaseContext().getSharedPreferences("userprefs", MODE_PRIVATE);
+                pref.edit().putString("username", null).commit();
+                finish();
+
+                Intent intent = new Intent(getApplicationContext(), IntentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
