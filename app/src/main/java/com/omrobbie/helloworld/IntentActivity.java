9045e271e87b57a78d5757fce946afd13714b80a
omrobbie <putu.roby@yahoo.com>
2017-05-17 19:15:32 +0800

    cara menggunakan SharedPreferences

--- app/src/main/java/com/omrobbie/helloworld/IntentActivity.java
+++ app/src/main/java/com/omrobbie/helloworld/IntentActivity.java
@@ -1,6 +1,7 @@
 package com.omrobbie.helloworld;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class IntentActivity extends AppCompatActivity {
+
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,27 @@ public class IntentActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final String userName = txtName.getText().toString();
 
-                // pindah ke layout yang lain dengan intent
-                Intent intent = new Intent(IntentActivity.this, IntentTargetActivity.class);
-                intent.putExtra("data", userName);
-                startActivity(intent);
+                // simpan data kedalam session (sharedpreference)
+                pref = getBaseContext().getSharedPreferences("userprefs", MODE_PRIVATE);
+                pref.edit().putString("username", userName).commit();
+
+                // jalankan fungsi gotoHome
+                gotoHome(userName);
             }
         });
+
+        // cek apakah ada userName di sharedpreference
+        String datauser = pref.getString("userName", null);
+
+        if (datauser != null) {
+            gotoHome(datauser);
+        }
+    }
+
+    private void gotoHome(String userName) {
+        // pindah ke layout yang lain dengan intent
+        Intent intent = new Intent(IntentActivity.this, IntentTargetActivity.class);
+        intent.putExtra("data", userName);
+        startActivity(intent);
     }
 }
