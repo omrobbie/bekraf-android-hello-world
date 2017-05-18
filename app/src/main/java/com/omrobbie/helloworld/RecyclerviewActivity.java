7c2003d1710943e12077fbb240fe012401c66cf4
omrobbie <putu.roby@yahoo.com>
2017-05-18 11:23:33 +0800

    bikin recyclerview layout dan class

---
+++ app/src/main/java/com/omrobbie/helloworld/RecyclerviewActivity.java
@@ -0,0 +1,13 @@
+package com.omrobbie.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyclerviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview);
+    }
+}
