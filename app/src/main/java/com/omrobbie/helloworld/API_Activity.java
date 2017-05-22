c8e589ae09fd3f02d26b5e0e43af6c67c7af12ca
omrobbie <putu.roby@yahoo.com>
2017-05-22 20:10:04 +0800

    bikin activity baru untuk testing menggunakan API

---
+++ app/src/main/java/com/omrobbie/helloworld/API_Activity.java
@@ -0,0 +1,13 @@
+package com.omrobbie.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class API_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_);
+    }
+}
